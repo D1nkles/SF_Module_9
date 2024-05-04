603a1180e588fed33f831e87f9d6b80cd04c6ea1
Ilia <isinetov@mail.ru>
2024-05-04 18:18:12 +0400

    Задание 9.3.14. Используя консольное решение из задачи 9.3.12, реализуйте лямбда-оператор во время вызова анонимного метода.

--- Task 9_3_12/Task 9_3_12.cs
+++ Task 9_3_12/Task 9_3_12.cs
@@ -5,10 +5,8 @@
         delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            ShowMessageDelegate showMessageDelegate = delegate (string _message)
-            {
-                Console.WriteLine(_message);
-            };
+            ShowMessageDelegate showMessageDelegate = (string _message) => Console.WriteLine(_message);
+            
             showMessageDelegate.Invoke("Hello World!");
             Console.Read();
         }

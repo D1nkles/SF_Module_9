b07326c297a3c39796a30d2d7ab15f02a4984c11
Ilia <isinetov@mail.ru>
2024-05-04 18:19:07 +0400

    Задание 9.3.15 Используя консольное решение из задачи 9.3.13, реализуйте лямбда-оператор во время вызова анонимного метода

--- Task 9_3_13/Task 9_3_13.cs
+++ Task 9_3_13/Task 9_3_13.cs
@@ -5,7 +5,7 @@
         delegate int RandomNumberDelegate();
         static void Main(string[] args)
         {
-            RandomNumberDelegate randomNumberDelegate = delegate { return new Random().Next(0, 100); };
+            RandomNumberDelegate randomNumberDelegate = () => { return new Random().Next(0, 100); };
             int result = randomNumberDelegate.Invoke();
             Console.WriteLine(result);
             Console.Read();

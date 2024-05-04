85a35f4580ca5defbbbc5f31e5876ec2ca0ab64c
Ilia <isinetov@mail.ru>
2024-05-04 18:12:28 +0400

    Задание 9.3.12.

---
+++ Task 9_3_12/Task 9_3_12.cs
@@ -0,0 +1,16 @@
+﻿namespace DelegatePractices
+{
+    class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+    }
+}

7a4e80717ba7c2bdde850e9949bdc83e623d688a
Ilia <isinetov@mail.ru>
2024-05-04 17:27:13 +0400

    Задание 9.3.2. Создайте консольное приложение, в котором есть функция, принимающая на вход два числа int, и возвращающая результат int вычитания второго числа из первого. Вызовите эту функцию в классе Main при помощи делегата и отобразите результат в консольном сообщении.

---
+++ Task 9_3_2/Task 9_3_2.cs
@@ -0,0 +1,14 @@
+﻿class Program 
+{
+    public delegate int myDelegate(int num1, int num2);
+    static int Subtraction(int num1, int num2) 
+    {
+        return num1 - num2;
+    }
+    static void Main(string[] args) 
+    {
+        myDelegate myDel = Subtraction;
+
+        Console.WriteLine(myDel.Invoke(10, 4));
+    }
+}

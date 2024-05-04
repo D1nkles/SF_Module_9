0c4692ccf14f21d26411ae34b16dd699d38e22ce
Ilia <isinetov@mail.ru>
2024-05-04 19:49:27 +0400

    Задание 9.4.2. На основе скринкаста создайте консольное приложение, в котором реализуйте демонстрацию ковариантности делегатов при помощи следующей модели классов:

---
+++ Task 9_4_2/Task 9_4_2.cs
@@ -0,0 +1,14 @@
+﻿class Program 
+{
+    public delegate Car CovarTestDel();
+    public static Lexus CovarTest() 
+    {
+        return null;
+    }
+    static void Main(string[] args) 
+    {
+        CovarTestDel covariancyDel = CovarTest;
+    }
+}
+class Car { }
+class Lexus : Car { }

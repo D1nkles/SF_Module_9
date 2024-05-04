018fe1a2a2152b2699852c4029ccab91633a05b1
Ilia <isinetov@mail.ru>
2024-05-04 16:49:32 +0400

    Задание 9.1.4. Создайте класс исключения Exception и переопределите его свойство Message, а также свойство HelpLink, добавив в него ссылку на внешний ресурс.

--- Task 9_1_3/Task 9_1_3.cs
+++ Task 9_1_3/Task 9_1_3.cs
@@ -1,3 +1,4 @@
-﻿var myException = new Exception();
+﻿var myException = new Exception("Сообщение об ошибке!!");
 myException.Data.Add("Дата создания исключения", DateTime.Now);
+myException.HelpLink = "www.youtube.com";
 Console.WriteLine(myException.Data["Дата создания исключения"]);

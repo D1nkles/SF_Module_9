7170f95d8003383f8ff1f03f638235e0faa57bec
Ilia <isinetov@mail.ru>
2024-05-04 17:41:55 +0400

    Задание 9.3.4. Реализуйте консольное приложение, в котором существует две функции: первая функция вычитает второе число из первого и отображает результат в консольном сообщении, вторая функция складывает два числа и отображает результат в консоли. Реализуйте вызов этих двух функций через многоадресный делегат.

---
+++ Task 9_3_4/Task 9_3_4.cs
@@ -0,0 +1,20 @@
+﻿class Program 
+{
+    public delegate void MyDelegate(int num1, int num2);
+
+    static void Subtraction(int num1, int num2)
+    {
+        Console.WriteLine($"Результат вычитания: {num1 - num2}");
+    }
+
+    static void Addition(int num1, int num2) 
+    {
+        Console.WriteLine($"Результат сложения: {num1 + num2}");
+    }
+    static void Main(string[] args) 
+    {
+        MyDelegate myDel = Subtraction;
+        myDel += Addition;
+        myDel.Invoke(10, 4);
+    }
+}
